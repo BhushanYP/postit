50a0db8f36c3ef83dbfa3eecb18347f0db75720f
Bhushan Patil <147179879+BhushanYP@users.noreply.github.com>
2026-04-27 13:50:09 +0530

    Delete postit directory

--- postit/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.home, name= 'home'),
-    path('home', views.home, name= 'home'),
-    path('sign-up', views.sign_up, name= 'sign-up'),
-    path('post', views.create_post, name='post')
-]
